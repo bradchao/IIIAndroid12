536b70603efbb85e0742a7f8e1a8df977a7a5e19
brad <brad@brad.tw>
2019-12-03 11:37:01 +0800

    7

--- app/src/main/java/com/example/iiiandroid12/P1.java
+++ app/src/main/java/com/example/iiiandroid12/P1.java
@@ -8,17 +8,41 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class P1 extends Fragment {
+    private View mainView;
+    private ViewFlipper viewFlipper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_p1, container, false);
+        if (mainView == null) {
+            mainView = inflater.inflate(R.layout.fragment_p1, container, false);
+            viewFlipper = mainView.findViewById(R.id.viewFlipper);
+            View f0 = viewFlipper.getChildAt(0);
+            View f1 = viewFlipper.getChildAt(1);
+            View f2 = viewFlipper.getChildAt(2);
+            View f3 = viewFlipper.getChildAt(3);
+            MyClickListener listener = new MyClickListener();
+            f0.setOnClickListener(listener);
+            f1.setOnClickListener(listener);
+            f2.setOnClickListener(listener);
+            f3.setOnClickListener(listener);
+        }
+
+        return mainView;
+    }
+
+    private class MyClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            viewFlipper.showNext();
+        }
     }
 
 }
